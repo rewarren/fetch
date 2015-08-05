be6005eeb28264b0b79f1049bca8523ed89dcf16
Rachel Warren <raelwa@gmail.com>
2015-08-05 16:58:03 -0400

    started styling

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,4 +16,13 @@ dug = Animal.create(name: "Dug", photo_url: "http://www.empireonline.com/images/
 
 garfield = Animal.create(name: "Garfield", photo_url: "http://images.mentalfloss.com/sites/default/files/styles/insert_main_wide_image/public/489346.png", age: 7, gender: "male", bio: "My human annoys me.", animal_type: "cat")
 
+Animal.create(name: "Beethoven", photo_url: "http://images.sodahead.com/polls/001622469/2631951034_01_wd0509_Beethoven_answer_6_xlarge.jpeg", age: 2, gender: "male", bio: "I'm a real family dog and I love getting into mischief!", animal_type: "dog")
+
+Animal.create(name: "Tweety", photo_url: "https://upload.wikimedia.org/wikipedia/en/d/db/Woodstock.png", age: 9, gender: "female", bio: "I like to hang with my pal Snoopy.", animal_type: "bird")
+
+Animal.create(name: "Fluffy", photo_url: "http://www.polyvore.com/cgi/img-thing?.out=jpg&size=l&tid=30867658", age: 50 , gender: "male", bio: "I'm a good guard dog for our home.  I also love falling asleep to the sound of music.", animal_type: "")
+
+Animal.create(name: "", photo_url: "", age: , gender: "", bio: "", animal_type: "")
+
+
 Like.create(animal: dug)

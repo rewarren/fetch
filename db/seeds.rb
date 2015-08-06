8ba8c959e85ebd842e5ca160d1cec70155af8d30
Rachel Warren <raelwa@gmail.com>
2015-08-06 19:59:04 -0400

    styling for root, index and show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,11 +11,11 @@ Animal.destroy_all
 snoopy = Animal.create(name: "Snoopy", photo_url: "http://img02.deviantart.net/08f5/i/2007/122/3/e/snoopy_dancing_by_jessie4508.jpg",
 age: 5, gender: "male", bio: "my best friend is a bird, my dog house takes me places! I love to eat and sleep.", animal_type: "dog", user_id: 1)
 
-dug = Animal.create(name: "Dug", photo_url: "http://www.empireonline.com/images/features/50-greatest-animated-characters/stills/10.jpg", age: 4, gender: "male", bio: "I love adventuSQUIRREL.", animal_type: "dog", user_id: 2)
+dug = Animal.create(name: "Dug", photo_url: "http://www.empireonline.com/images/features/50-greatest-animated-characters/stills/10.jpg", age: 4, gender: "male", bio: "I'm loyal and I love adventuSQUIRREL.", animal_type: "dog", user_id: 2)
 
-garfield = Animal.create(name: "Garfield", photo_url: "http://images.mentalfloss.com/sites/default/files/styles/insert_main_wide_image/public/489346.png", age: 7, gender: "male", bio: "My human annoys me.", animal_type: "cat", user_id: 3)
+garfield = Animal.create(name: "Garfield", photo_url: "http://images.mentalfloss.com/sites/default/files/styles/insert_main_wide_image/public/489346.png", age: 7, gender: "male", bio: "Give me all the lasagna", animal_type: "cat", user_id: 3)
 
-beethoven = Animal.create(name: "Beethoven", photo_url: "http://images.sodahead.com/polls/001622469/2631951034_01_wd0509_Beethoven_answer_6_xlarge.jpeg", age: 2, gender: "male", bio: "I'm a real family dog and I love getting into mischief!", animal_type: "dog", user_id: 4)
+beethoven = Animal.create(name: "Beethoven", photo_url: "http://images.sodahead.com/polls/001622469/2631951034_01_wd0509_Beethoven_answer_6_xlarge.jpeg", age: 2, gender: "male", bio: "I'm a real family man and I love getting into mischief!", animal_type: "dog", user_id: 4)
 
 tweety = Animal.create(name: "Tweety", photo_url: "https://upload.wikimedia.org/wikipedia/en/d/db/Woodstock.png", age: 9, gender: "female", bio: "I like to hang with my pal Snoopy.", animal_type: "bird", user_id: 5)
 

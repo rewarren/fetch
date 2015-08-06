011f963b5514ced357351f49c596c15800a13100
Rachel Warren <raelwa@gmail.com>
2015-08-06 16:15:20 -0400

    comment out activerecord-regulation-system in animal_like.rb

--- app/models/animal_like.rb
+++ app/models/animal_like.rb
@@ -2,8 +2,8 @@ class AnimalLike < ActiveRecord::Base
   belongs_to :animal
   belongs_to :user
 
-  validate_uniqueness_of :animal_id, scope: :user_id
-  validates_inclusion_of :value, in: [1, -1]
+  # validate_uniqueness_of :animal_id, scope: :user_id
+  # validates_inclusion_of :value, in: [1, -1]
   # validate: ensure_not_author
 
 end
